978b46f359c82dc6e53fee76928659db9d81b81e
tom <tommy.yohanes@icloud.com>
2026-05-16 13:42:57 +0700

    Initial Commit

---
+++ Nemonic/NemonicApp.swift
@@ -0,0 +1,17 @@
+//
+//  NemonicApp.swift
+//  Nemonic
+//
+//  Created by tomtomtom on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct NemonicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NemonicTests/NemonicTests.swift
@@ -0,0 +1,17 @@
+//
+//  NemonicTests.swift
+//  NemonicTests
+//
+//  Created by tomtomtom on 5/16/26.
+//
+
+import Testing
+@testable import Nemonic
+
+struct NemonicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
